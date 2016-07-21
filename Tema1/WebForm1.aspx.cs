73f85eeb6a934499975687c43008fc3ea0cb5261
dunarobert <dunarobert7@gmail.com>
2016-07-21 10:34:44 +0300

    Tema1 Update

---
+++ Tema1/WebForm1.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Tema1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string CS = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(CS))
+            {
+                SqlCommand cmd = new SqlCommand("Select CityId, CityName, Country from City", con);
+                con.Open();
+                DropDownList1.DataSource = cmd.ExecuteReader();
+                 DropDownList1.DataBind();
+
+            }
+        }
+    }
+}
